4bd2c6bd7916d1b049352cb90d354456d2388514
K-Minhyeok <22000082@handong.ac.kr>
2025-07-25 23:20:53 +0900

    Feat:Two-Bytes-Keyed XOR added

--- deobfuscator.py
+++ deobfuscator.py
@@ -3,8 +3,8 @@ import base64
 import re
 
 ASCII_LEN = 127-33
-passwd = "68656c6c6f"
-input_is_hex = True
+passwd = "1q2w3e4r"
+input_is_hex = False
 target_word = "kgfi"
 
 def is_hex(s):
@@ -33,12 +33,17 @@ def has_target_word(test_word):
     else :
         return False
 
-
 def get_result_of(test_password,where):
         result = zxcvbn(test_password)
         print(f"{where} : {test_password} / {result['score']} ")
         return result['password'], result['score']
 
+
+def check_out_of_range(test_word):
+    for c in test_word:
+        if ord(c)<32 or 126<ord(c):
+            return True
+    return False
 
 def deobf_with_ROT():
     
@@ -46,13 +51,19 @@ def deobf_with_ROT():
         test_passwd = list(passwd)
         
         for j in range(len(test_passwd)):
-            test_passwd[j] = chr(((ord(test_passwd[j]) - 33 + i) % 94) + 33)
+            test_passwd[j] = chr(((ord(test_passwd[j]))+i))
+            # test_passwd[j] = chr(((ord(test_passwd[j]) - 33 + i) % 94) + 33)
+
+        if check_out_of_range(test_passwd):
+            continue
 
         test_passwd = ''.join(test_passwd)
+
         if has_target_word(test_passwd):
             break
-
+        
         get_result_of(test_passwd,"ROT")
+
 
 def decode_with_Base64():
     try:
@@ -61,20 +72,47 @@ def decode_with_Base64():
     except Exception:
         print("It's not Base64 Encoded")
         return None 
-
-def deobf_with_XOR_noKeyed():
-
+    
+def deobf_with_XOR_one_key():
 
     for i in range(256):
         test_passwd = list(passwd)
+
         for j in range(len(test_passwd)):
-            test_passwd[j] = chr( ((ord(test_passwd[j]) - 33) ^ i) % 94 + 33 )
-        
+            test_passwd[j] = chr(((ord(test_passwd[j])^ i)))
+
+        if check_out_of_range(test_passwd):
+            continue
+
         test_passwd = ''.join(test_passwd)
+
         if has_target_word(test_passwd):
             break
-
         get_result_of(test_passwd,"XOR KEY_x")
+
+
+def deobf_with_XOR_two_bytes_keys():
+    count =0
+    for i in range(256):
+
+        for j in range(256):
+            test_passwd = list(passwd)  
+            for c in range(len(test_passwd)):
+                key = i if c%2==0 else j
+                test_passwd[c] = chr(((ord(test_passwd[c])^ key)))
+
+            if check_out_of_range(test_passwd):
+                continue
+
+            test_passwd = ''.join(test_passwd)
+
+            if has_target_word(test_passwd):
+                break
+
+            print(count,end=' ')
+            get_result_of(test_passwd,"XOR 2Bytes_Key")    
+            count+=1 
+
 
 if input_is_hex:
     passwd = convert_to_bytes(passwd)
@@ -82,5 +120,5 @@ if input_is_hex:
 
 # deobf_with_ROT()
 # print(decode_with_Base64())
-# deobf_with_XOR_noKeyed()
-
+# deobf_with_XOR_one_key()
+deobf_with_XOR_two_bytes_keys()
